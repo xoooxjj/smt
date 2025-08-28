da3bda53a41329519d05c5cacfc99a4e636a28c4
xoooxjj <kenny_lee7@me.com>
2025-08-28 11:55:02 +0800

    smt2.0.php

--- smt2.0.php
+++ smt2.0.php
@@ -9,8 +9,8 @@ date_default_timezone_set("Asia/Shanghai");
 const CONFIG = [
     'upstream'   => [
     'http://198.16.100.186:8278/',
-    'http://50.7.92.106:8278/', 
-    'http://50.7.234.10:8278/',
+    'http://50.7.158.194:8278/', 
+    'http://50.7.92.106:8278/',
     'http://50.7.220.170:8278/',
     'http://67.159.6.34:8278/'],
     'list_url'   => 'https://cdn.jsdelivr.net/gh/hostemail/cdn@main/data/smart.txt',
@@ -393,4 +393,5 @@ function fetchUrl($url) {
 function getBaseUrl() {
     return (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? 'https' : 'http') 
            . "://$_SERVER[HTTP_HOST]";
+
 }
